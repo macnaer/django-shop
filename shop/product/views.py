49f104fecc6af63667d072db569e23606c20e34c
k2 <trofimchuk.an@gmail.com>
2023-06-27 19:25:43 +0300

    Update paginator

--- shop/product/views.py
+++ shop/product/views.py
@@ -5,8 +5,6 @@ from functools import wraps
 from users.models import CustomUser, Order_basket
 
 
-
-
 def context_data(func):
     @wraps(func)
     def wrapper(request, *args, **kwargs):
@@ -15,8 +13,7 @@ def context_data(func):
         user = CustomUser.objects.order_by('username')
         user_id = Order_basket.objects.order_by('id')
 
-        paginator = Paginator(products, 2)
-        print(f"paginator {paginator}")
+        paginator = Paginator(products, 20)
         page_number = request.GET.get('page')
         page = paginator.get_page(page_number)
 
@@ -24,12 +21,11 @@ def context_data(func):
             "products": page,
             'categories': categories,
             'users': user,
-            'user_id':user_id,
-            
+            'user_id': user_id,
+
         }
         return func(request, *args, context=context, **kwargs)
     return wrapper
-
 
 
 # def context_data(func):
@@ -49,21 +45,20 @@ def context_data(func):
 #     return wrapper
 
 
-
-
-
-
 @context_data
 def index(request, context):
     return render(request, 'pages/index.html', context)
+
 
 @context_data
 def shop(request, context):
     return render(request, 'pages/shop.html', context)
 
+
 @context_data
 def checkout(request, context):
     return render(request, 'pages/checkout.html', context)
+
 
 @context_data
 def contact(request, context):
@@ -71,30 +66,31 @@ def contact(request, context):
 
 
 @context_data
-def cart(request, context):   
-   
+def cart(request, context):
+
     return render(request, 'pages/cart.html', context)
+
 
 @context_data
 def products_by_category(request, category_id, context):
     category = get_object_or_404(Category, id=category_id)
     products = Product.objects.filter(category=category).order_by('name')
     categories = Category.objects.order_by('name')
-    
+
     context = {
         'category': category,
-        'categories':categories,
+        'categories': categories,
         'products': products,
     }
     return render(request, 'pages/products_by_category.html', context)
 
 
 @context_data
-def detail(request, product_id ,context):
-    products = get_object_or_404(Product, id=product_id)      
+def detail(request, product_id, context):
+    products = get_object_or_404(Product, id=product_id)
     categories = Category.objects.order_by('name')
-    context = {       
-        'categories':categories,
+    context = {
+        'categories': categories,
         'products': products,
     }
     return render(request, 'pages/detail.html', context)
